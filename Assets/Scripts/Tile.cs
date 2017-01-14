324e098d0461611536f31a00bbac5251276b1177
jpinto <jarrydpinto@gmail.com>
2017-01-14 12:53:05 +1100

    Created initial level and level loader.

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+    public Sprite tile_ground, tile_path;
+    public enum tileType
+    {
+        GROUND,
+        PATH
+    };
+
+    public void setTile(tileType type)
+    {
+        switch (type)
+        {
+            case tileType.GROUND:
+                gameObject.GetComponent<SpriteRenderer>().sprite = tile_ground;
+            break;
+            case tileType.PATH:
+                gameObject.GetComponent<SpriteRenderer>().sprite = tile_path;
+            break;
+        }
+    }
+}
