2598a97b3cb14480be0ff6eb2301eafe4469e4c0
Joseph Tran <valrigardian@gmail.com>
2017-01-14 11:54:53 +1100

    Add waypoint movement stuff

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject[] enemies;
+	public GameObject[] waypoints;
+	public float spawnSpeed = 1.0f;
+
+	private float lastSpawnedTime = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - lastSpawnedTime > spawnSpeed) {
+			var enemy = Instantiate (enemies [Random.Range (0, enemies.Length - 1)]) as GameObject;
+			enemy.transform.position = transform.position;
+			enemy.GetComponent<WaypointMovement>().waypoints = waypoints;
+			lastSpawnedTime = Time.time;
+		}
+		
+	}
+}
